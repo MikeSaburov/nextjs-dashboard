56b3b49b742b7db63a321cee0626bbda00b073a7
Mikhail Saburov <saburov1985@gmail.com>
2024-01-05 22:51:57 +1100

    refactor(fonts): добавил толщину шрифта

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,7 @@
 import { Inter, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['cyrillic', 'latin'] });
-export const lusitana = Lusitana({ subsets: ['latin'], weight: '700' });
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
