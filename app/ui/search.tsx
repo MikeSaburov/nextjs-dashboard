bce0d3339b7923dcadacd4d23d41fdb9f0daffbb
Mikhail Saburov <saburov1985@gmail.com>
2024-01-20 02:33:35 +1100

    feature(search): передаем введеные данные в параметры url

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -5,15 +5,18 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
   function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
+
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
-    console.log(params);
+    replace(`${pathname}?${params.toString()}`);
   }
 
   return (
